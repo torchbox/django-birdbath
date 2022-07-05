52cfd608d805e506dd2264a2b9507c95ce97a647
Tom Usher <tom@tomusher.com>
2022-07-05 17:15:39 +0100

    Add default_auto_field

--- birdbath/apps.py
+++ birdbath/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class BirdBathConfig(AppConfig):
     name = "birdbath"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         from . import system_checks
